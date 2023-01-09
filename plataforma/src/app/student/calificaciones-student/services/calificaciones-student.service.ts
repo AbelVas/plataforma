7a8f7380c5e3155bdc0bb712c5acfe68c4799856
DavidQuezada <quezadita0409@gmail.com>
2023-01-09 07:07:37 -0600

    Final ->David

--- plataforma/src/app/student/calificaciones-student/services/calificaciones-student.service.ts
+++ plataforma/src/app/student/calificaciones-student/services/calificaciones-student.service.ts
@@ -8,7 +8,7 @@ import {map,tap,catchError, mergeScan} from 'rxjs/operators'
 })
 export class CalificacionesStudentService {
 
-  URL='http://localhost:3002';
+  URL='https://app.labrincoteca.edu.gt/';
 
   constructor(private http:HttpClient) { }
 
