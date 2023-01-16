f1b09b111814815cb1d95b6643a1677b936201ca
abel.vasquez12@gmail.com <abel.vasquez12@gmail.com>
2023-01-16 17:56:39 -0600

    actualización BUild

--- servidor/build/config/keys.js
+++ servidor/build/config/keys.js
@@ -5,6 +5,6 @@ exports.default = {
         host: '204.93.224.100',
         user: 'flrsjejd_plataforma',
         password: 'macromish40322454',
-        database: 'flrsjejd_plataformaplanbasico'
+        database: 'flrsjejd_plataformaplanbasico_test'
     }
 };
