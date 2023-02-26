7dd3c6962c452f7aa7094392f6d6b376f4c35dba
KariKariaaa <dianakarinapaola@gmail.com>
2023-02-25 21:18:51 -0600

    Arreglos

--- plataforma/src/app/admin/estadistica/estadistica.component.ts
+++ plataforma/src/app/admin/estadistica/estadistica.component.ts
@@ -9,19 +9,12 @@ Chart.register(...registerables);
   styleUrls: ['./estadistica.component.css']
 })
 export class EstadisticaComponent implements OnInit {
-<<<<<<< HEAD
   //Chart de niños y niñas
   myChart:any;
-=======
-  totalNinos:any=''
-  totalNinas:any=''
-
->>>>>>> acfa93b1a1ac01bd64ab7d6302a2e6a276b1b460
   chartdata: any;
   labeldata: any[] = [];
   realdata: any[] = [];
 
-<<<<<<< HEAD
   //Chart de codigos
   chartCodigos:any;
   dataCodigos: any;
@@ -33,45 +26,19 @@ export class EstadisticaComponent implements OnInit {
   dataContrase: any;
   labelContrase: any[] = [];
   realContrase: any[] = [];
-=======
-  chartdata1: any;
-  labeldata1: any[] = [];
-  realdata1: any[] = [];
-
-  chartdata2: any;
-  labeldata2: any[] = [];
-  realdata2: any[] = [];
-
-  chartdata3: any;
-  labeldata3: any[] = [];
-  realdata3: any[] = [];
-
-  chartdata4: any;
-  labeldata4: any[] = [];
-  realdata4: any[] = [];
->>>>>>> acfa93b1a1ac01bd64ab7d6302a2e6a276b1b460
   constructor(private estadisticaService:EstadisticasDashboardService, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
 
-<<<<<<< HEAD
     //Chart Alumnos Niño/Niña
     this.estadisticaService.Prueba().subscribe((result: any) => {
-=======
-    this.estadisticaService.Prueba().subscribe((result: any) => { //Aquí es donde se jala el servicio
->>>>>>> acfa93b1a1ac01bd64ab7d6302a2e6a276b1b460
       this.chartdata = result;
       if(this.chartdata!=null){
         //configuracion para la info de las charts
         for(let i=0; i<this.chartdata.length ;i++){
-<<<<<<< HEAD
           //console.log(this.chartdata[i]);
           //this.labeldata.push(this.chartdata[i].sexo);
           this.realdata.push(this.chartdata[i].CantidadAlumno);
-=======
-          this.labeldata.push(this.chartdata[i].nombre_grado); //este es el titulado de los datos (No necesario en algunos casos)
-          this.realdata.push(this.chartdata[i].CantidadAlumno); //lenght de niño y length de niña
->>>>>>> acfa93b1a1ac01bd64ab7d6302a2e6a276b1b460
         }
        }
       }
@@ -104,7 +71,6 @@ export class EstadisticaComponent implements OnInit {
   }
 //esto es de mi parte
   RenderChart(labeldata: any[], realdata: any[]) {
-<<<<<<< HEAD
     let htmlRef = this.elementRef.nativeElement.querySelector(`#prueba`);
       this.myChart = new Chart(htmlRef, {
         type: 'pie',
@@ -156,20 +122,6 @@ export class EstadisticaComponent implements OnInit {
           }
         }
     });
-=======
-  const myChart = new Chart('pie1', {
-  type: 'pie',
-  data: {
-    labels: ['niño','niña'],
-    datasets: [{
-      label: 'Estudiantes por grado',
-      data: realdata,
-    }]
-  },
-  options: {
-    scales: {
-    }
->>>>>>> acfa93b1a1ac01bd64ab7d6302a2e6a276b1b460
   }
 
 }
