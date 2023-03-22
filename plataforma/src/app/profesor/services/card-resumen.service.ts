72181ae074acb3f2cdf938bcd04d618a3e5ce63b
KariKariaaa <dianakarinapaola@gmail.com>
2023-03-21 23:59:26 -0600

    Notas bimestrales prof y orden de alumnos califica

--- plataforma/src/app/profesor/services/card-resumen.service.ts
+++ plataforma/src/app/profesor/services/card-resumen.service.ts
@@ -63,6 +63,52 @@ export class CardResumenService {
     );
   }
 
+  //consolidado bimestral ADMINISTRADORES/PROFESORES
+  getAlumnosGradoCuadroGuia(idGrado:string){
+    const httpOptions={headers:new HttpHeaders({'Auth-Token':`${localStorage['Acces-Token']}`})}
+    return this.http.get(`${this.URL}/cuadroguia/alumnos/${idGrado}`,httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+ getActividadesCurso(idCurso:string,idUnidad:string){
+  const httpOptions={headers:new HttpHeaders({'Auth-Token':`${localStorage['Acces-Token']}`})}
+  return this.http.get(`${this.URL}/cuadroguia/actividades-curso/${idCurso}/${idUnidad}`,httpOptions).pipe(
+    catchError(this.handleError)
+  );
+ }
+ getNotasFinal(idCurso:string,idUnidad:string,idGrado:string,){
+  const httpOptions={headers:new HttpHeaders({'Auth-Token':`${localStorage['Acces-Token']}`})}
+  return this.http.get(`${this.URL}/cuadroguia/${idCurso}/${idUnidad}/${idGrado}`,httpOptions).pipe(
+    catchError(this.handleError)
+  );
+ }
+  cursoDocente(idCruso:string){
+    const httpOptions={headers:new HttpHeaders({'Auth-Token':`${localStorage['Acces-Token']}`})}
+    return this.http.get(`${this.URL}/cursos/curso-profesor-individual/${idCruso}`,httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  //CUADRO GUIA
+  obtenerGradoSeccion(idGrado:string){
+    const httpOptions={headers:new HttpHeaders({'Auth-Token':`${localStorage['Acces-Token']}`})}
+    return this.http.get(`${this.URL}/grados/grado-seccion-nivel-jornada/${idGrado}`,httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+  obtenerCursosGrado(idGrado:string){
+    const httpOptions={headers:new HttpHeaders({'Auth-Token':`${localStorage['Acces-Token']}`})}
+    return this.http.get(`${this.URL}/cuadroguia/cursos-cuadro-guia/${idGrado}`,httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+  obtenerNotasCuadroGuia(idGrado:string,idUnidad:string){
+    const httpOptions={headers:new HttpHeaders({'Auth-Token':`${localStorage['Acces-Token']}`})}
+    return this.http.get(`${this.URL}/cuadroguia/${idGrado}/${idUnidad}`,httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
   private handleError(error:HttpErrorResponse){
     var msg={};
     if(error.status==400){
