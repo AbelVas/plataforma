80ad3cac5913acee06d80f33df0e3d6b35285cc1
abel.vasquez12@gmail.com <abel.vasquez12@gmail.com>
2023-03-22 09:00:37 -0600

    actualización build server

---
+++ servidor/build/routes/boletas.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const session_1 = require("../middleware/session");
+const boletas_1 = require("../controllers/boletas");
+const router = (0, express_1.Router)();
+exports.router = router;
+router.get("/:idGrado/:idAlumno", session_1.checkJwt, boletas_1.alumnosGrado);
+router.get("/boleta-final/:idGrado/:idAlumno", session_1.checkJwt, boletas_1.alumnoNotasBoleta);
