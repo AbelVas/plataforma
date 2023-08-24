b0d6ae04582372a9cf5bd50ab0d3f805b2353afa
abel.vasquez12@gmail.com <abel.vasquez12@gmail.com>
2023-08-23 22:32:47 -0600

    Inicio operación plugin

---
+++ servidor/src/controllers/menuDinamico.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import conexion from "../config/database";
+
+const menuAlumnosGet=async(req:Request,res:Response)=>{
+
+}
+const menuAdminGet=async(req:Request,res:Response)=>{
+    
+}
+const menuProfesorGet=async(req:Request,res:Response)=>{
+    
+}
+const menuSecretariaGet=async(req:Request,res:Response)=>{
+    
+}
+
+export {menuAlumnosGet,menuAdminGet,menuProfesorGet,menuSecretariaGet}
