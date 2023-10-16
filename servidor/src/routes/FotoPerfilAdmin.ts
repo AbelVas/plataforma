7d136983d21ee2f2d1799c5e59cb29f601add9c1
abel.vasquez12@gmail.com <abel.vasquez12@gmail.com>
2023-10-16 06:43:47 -0600

    correcciones varias

--- servidor/src/routes/FotoPerfilAdmin.ts
+++ servidor/src/routes/FotoPerfilAdmin.ts
@@ -3,7 +3,9 @@ import { checkJwt } from "../middleware/session";
 import { handleFileUpload } from "../utils/FotoPerfilAdmin";
 
 const router=Router();
-
+router.get("/profil-admin/photo",(req,res)=>{
+    res.send({jala:"ahueso"})
+})
 router.post("/profil-admin/photo",handleFileUpload,(req,res)=>{
     res.json({message: 'Archivo Subido Correctamente'})
 });
