cf471ad92707cc1c9e96fe944fa2838eb27da96c
abel.vasquez12@gmail.com <abel.vasquez12@gmail.com>
2024-03-17 13:19:58 -0600

    instalación de socket.io

--- servidor/src/app.ts
+++ servidor/src/app.ts
@@ -1,10 +1,12 @@
 import "dotenv/config"
 import express from "express"
+import http from 'http'
 import {router} from "./routes"
-
-
+import { Server as WebSocketServer} from "socket.io"
 const PORT=process.env.PORT || 3000
 const app=express();
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 const cors=require('cors');
@@ -12,5 +14,10 @@ app.use(cors(
     //acá se puede determinar que origenes y que no se puede conectar a nuestra api o consumir sus recursos
 ));
 
+
+const server=http.createServer(app)
+const httpServer=server.listen(PORT)
+const io=new WebSocketServer(server)
+
 app.use(router);
-app.listen(PORT)
+
