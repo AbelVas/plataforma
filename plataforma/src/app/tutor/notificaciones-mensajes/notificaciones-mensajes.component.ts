da5664f96afd3fac3eaf19c9e1cd1c021e473890
quezzaa <quezadita0409@gmail.com>
2024-08-05 20:29:47 -0600

    Arreglado?

---
+++ plataforma/src/app/tutor/notificaciones-mensajes/notificaciones-mensajes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notificaciones-mensajes',
+  templateUrl: './notificaciones-mensajes.component.html',
+  styleUrls: ['./notificaciones-mensajes.component.css']
+})
+export class NotificacionesMensajesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
